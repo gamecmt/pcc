30eaeb67745dc6363e474d08dde94d313d579932
gamecmt <gamecmt@gmail.com>
2017-01-17 16:38:09 +0800

    5-1 : 5-7

--- chapter5/cars.py
+++ chapter5/cars.py
@@ -52,18 +52,50 @@ if 'extra cheese' in requested_toppings:
 print("\nFinished making your pizza!")
 
 # 5-3
-alien_color = ['green', 'yellow', 'red']
-if 'green' in alien_color:
+alien_color = 'green'
+if alien_color == 'green':
     print("you won 5.")
 
 # 5-4
-if 'green' in alien_color:
+alien_color = 'red'
+if alien_color == 'green':
     print("you won 5.")
 else:
     print("you won 10.")
 
 # 5-5
-if 'green' in alien_color:
+alien_color = 'red'
+if alien_color == 'green':
     print("you won 5.")
-elif:
+elif alien_color == 'yellow':
     print("you won 10.")
+elif alien_color == 'red':
+    print("you won 15")
+
+# 5-6
+age = 5
+if age < 2:
+    print("baby")
+elif age < 4:
+    print("toddle")
+elif age < 13:
+    print("children")
+elif age < 20:
+    print("teenager")
+elif age < 65:
+    print("adult")
+elif age >= 65:
+    print("old people")
+
+# 5-7
+favortie_fruits = ['bananas', 'apples', 'oranges']
+if 'apples' in favortie_fruits:
+    print("You really like apples!")
+if 'bananas' in favortie_fruits:
+    print("You really like apples!")
+if 'oranges' in favortie_fruits:
+    print("You really like apples!")
+if 'pears' in favortie_fruits:
+    print("You really like pears!")
+if 'pineapples' in favortie_fruits:
+    print("You really like pineapples!")
