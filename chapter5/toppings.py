7b3d898bc8c6804a111c2d544059ee3f049eca9d
gamecmt <gamecmt@gmail.com>
2017-01-18 09:06:11 +0800

    5-9 : 5-11

--- chapter5/toppings.py
+++ chapter5/toppings.py
@@ -36,9 +36,10 @@ for user in users:
     else:
         print("Hello " + user + ", thank you for logging in again.")
 
+print("******************")
 # 5-9
 users = []
-if user is null:
+if not users:
     print("We need to find some users!")
 else:
     for user in users:        
@@ -46,3 +47,24 @@ else:
             print("Hello admin, would you like to see a status report?")
         else:
             print("Hello " + user + ", thank you for logging in again.")
+
+# 5-10
+current_users=['admin','ak47','f117','cmt','peiandsky','flypig']
+new_users=['laohu','jiaji','hao','ak47','f117']
+for new_user in new_users:
+    if new_user in current_users:
+        print("The name "+new_user+" was used, please choose another.")
+    else:
+        print("The name "+new_user+" is not used.")
+
+# 5-11
+nums=list(range(1,10))
+for num in nums:
+    if num==1:
+        print("1st")
+    elif num==2:
+        print("2nd")
+    elif num==3:
+        print("3rd")
+    else:
+        print(str(num)+"th")
